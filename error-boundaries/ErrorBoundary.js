d30e8b2989972ef46ef5b704c0f1efbc281ea3f3
bhawna90 <gaurbhawna90@gmail.com>
2023-03-13 21:57:54 +0530

    Create ErrorBoundary.js

---
+++ error-boundaries/ErrorBoundary.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+class ErrorBoundary extends React.Component{
+    constructor(props){
+        super(props)
+        this.state={
+            hasError: false
+        }
+    }
+    static getDerivedStateFromError(error){
+        return {
+            hasError: true
+        }
+    }
+    componentDidCatch(error, info){
+        console.log(error);
+        console.log(info);
+    }
+    render(){
+        console.log(this.props.value);
+        if(this.state.hasError)
+            return <div>Something went wrong</div>
+        return this.props.children
+        
+    }
+}
+
+export default ErrorBoundary
